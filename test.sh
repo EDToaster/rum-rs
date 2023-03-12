0a1b06ad9380101b38211d1c34ada9fe61f8cdce
EDToaster <howard3061@gmail.com>
2023-03-12 22:07:57 +0000

    Fixer upper test script

--- test.sh
+++ test.sh
@@ -2,7 +2,18 @@
 
 set -euo pipefail
 
-rum spinner -s circle -t "Initializing quantum flux capacitors..." -- sleep 2
+rum spinner -s circle -t "Initializing quantum flux capacitors..." -- sleep 2 
 rum spinner -s monkey -t "Warning: Don't set yourself on fire." -- sleep 1
 rum spinner -s meter -t "Consuming all those consumables..." -- sleep 2
 
+rum typer -t "Game is ready ..." -w 2000 -i 50
+
+rum confirm -n "No thank you" -y "🪙" -t "Insert coin ..." || { rum typer -t "Sorry to see you go!" -w 2000 -i 50 ; exit ; }
+
+USERNAME=$(rum text -p "What is your name?")
+
+DIFF=$(printf "Easy\nMedium\nHard" | rum choose -t "Choose a difficulty, ${USERNAME}")
+
+rum typer -t "Must've been a ${DIFF} decision." -w 1000 -i 50
+rum typer -t "Well that's it!" -w 1000 -i 50
+rum typer -t "Thanks for playing, ${USERNAME}." -w 1000 -i 50
